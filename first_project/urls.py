739e38846363c7540c3d9b23645080cdb6acce68
Abhishek <abhishekdubey806@gmail.com>
2019-09-25 23:32:07 +0530

    adding model for user and profile picture field made migrations -> database updated -> signals.py to create profiles automatically with new signups --- no need to do it manually now

--- first_project/urls.py
+++ first_project/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
+
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/',user_views.register,name = 'register'),
@@ -28,3 +31,5 @@ urlpatterns = [
     #url matching with blog/.......
     # path left empty so now its our homepage
 ]
+if settings.DEBUG:
+    urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
