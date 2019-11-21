9366e6642e47305c66bbaeded36fc032e691dbf1
Abhishek <abhishekdubey806@gmail.com>
2019-11-21 18:46:25 +0530

    just some url fixes

--- blog/views.py
+++ blog/views.py
@@ -139,7 +139,7 @@ class SearchView(ListView):
 #         return object_list
 
 #----------------------------------------------------------------------------
-
+@login_required
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
